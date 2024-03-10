a4f0d90b08bb43cfea6f888759f39d83832d9b80
bino <hoangpham23052003@gmail.com>
2024-03-10 20:36:41 +0700

    Clear unnecessary routes and view files

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { Head, Link } from "@inertiajs/vue3";
+
+defineProps({});
+</script>
+
+<template>
+    <Head title="Social Media Website" />
+</template>
+
+<style></style>
