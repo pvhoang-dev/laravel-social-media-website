41ce908e22a90a67a29b7d6ffb3005ff380add2b
bino <hoangpham23052003@gmail.com>
2024-03-17 00:57:45 +0700

    Implement preview of the attachments

--- resources/js/Components/app/PostList.vue
+++ resources/js/Components/app/PostList.vue
@@ -3,6 +3,7 @@ import PostItem from "@/Components/app/PostItem.vue";
 import PostModal from "@/Components/app/PostModal.vue";
 import { ref } from "vue";
 import { usePage } from "@inertiajs/vue3";
+import AttachmentPreviewModal from "@/Components/app/AttachmentPreviewModal.vue";
 
 defineProps({
     posts: Array,
@@ -10,10 +11,21 @@ defineProps({
 
 const authUser = usePage().props.auth.user;
 const showEditModal = ref(false);
+const showAttachmentsModal = ref(false);
 const editPost = ref({});
+const previewAttachmentsPost = ref({});
+
 function openEditModal(post) {
     editPost.value = post;
     showEditModal.value = true;
+}
+
+function openAttachmentPreviewModal(post, index) {
+    previewAttachmentsPost.value = {
+        post,
+        index,
+    };
+    showAttachmentsModal.value = true;
 }
 
 function onModalHide() {
@@ -32,12 +44,19 @@ function onModalHide() {
             :key="post.id"
             :post="post"
             @editClick="openEditModal"
+            @attachmentClick="openAttachmentPreviewModal"
         />
 
         <PostModal
             :post="editPost"
             v-model="showEditModal"
             @hide="onModalHide"
+        />
+
+        <AttachmentPreviewModal
+            :attachments="previewAttachmentsPost.post?.attachments || []"
+            v-model:index="previewAttachmentsPost.index"
+            v-model="showAttachmentsModal"
         />
     </div>
 </template>
