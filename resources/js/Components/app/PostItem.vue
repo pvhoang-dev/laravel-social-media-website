255f6a241a60fa1a2da7481489fd69fe8fe59f03
bino <hoangpham23052003@gmail.com>
2024-03-20 22:17:30 +0700

    Create reusable CommentList.vue component for subcomments

--- resources/js/Components/app/PostItem.vue
+++ resources/js/Components/app/PostItem.vue
@@ -1,26 +1,20 @@
 <script setup>
 import {
-    ChatBubbleLeftRightIcon,
     ChatBubbleLeftEllipsisIcon,
     HandThumbUpIcon,
 } from "@heroicons/vue/24/outline";
 import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
-import { ref } from "vue";
 import PostUserHeader from "@/Components/app/PostUserHeader.vue";
 import { router, usePage } from "@inertiajs/vue3";
 import axiosClient from "@/axiosClient.js";
-import InputTextarea from "@/Components/InputTextarea.vue";
-import IndigoButton from "@/Components/app/IndigoButton.vue";
 import ReadMoreReadLess from "@/Components/app/ReadMoreReadLess.vue";
 import EditDeleteDropdown from "@/Components/app/EditDeleteDropdown.vue";
 import PostAttachments from "@/Components/app/PostAttachments.vue";
+import CommentList from "@/Components/app/CommentList.vue";
 
-const authUser = usePage().props.auth.user;
-const editingComment = ref(null);
 const props = defineProps({
     post: Object,
 });
-const newCommentText = ref("");
 const emit = defineEmits(["editClick", "attachmentClick"]);
 
 function openEditModal() {
@@ -48,68 +42,6 @@ function sendReaction() {
             props.post.current_user_has_reaction =
                 data.current_user_has_reaction;
             props.post.num_of_reactions = data.num_of_reactions;
-        });
-}
-
-function createComment() {
-    axiosClient
-        .post(route("post.comment.create", props.post), {
-            comment: newCommentText.value,
-        })
-        .then(({ data }) => {
-            newCommentText.value = "";
-            props.post.comments.unshift(data);
-            props.post.num_of_comments++;
-        });
-}
-
-function deleteComment(comment) {
-    if (!window.confirm("Are you sure you want to delete this comment?")) {
-        return false;
-    }
-    axiosClient
-        .delete(route("post.comment.delete", comment.id))
-        .then(({ data }) => {
-            props.post.comments = props.post.comments.filter(
-                (c) => c.id !== comment.id
-            );
-            props.post.num_of_comments--;
-        });
-}
-
-function startCommentEdit(comment) {
-    console.log(comment);
-    editingComment.value = {
-        id: comment.id,
-        comment: comment.comment.replace(/<br\s*\/?>/gi, "\n"),
-    };
-}
-
-function updateComment() {
-    axiosClient
-        .put(
-            route("comment.update", editingComment.value.id),
-            editingComment.value
-        )
-        .then(({ data }) => {
-            editingComment.value = null;
-            props.post.comments = props.post.comments.map((c) => {
-                if (c.id === data.id) {
-                    return data;
-                }
-                return c;
-            });
-        });
-}
-
-function sendCommentReaction(comment) {
-    axiosClient
-        .post(route("comment.reaction", comment.id), {
-            reaction: "like",
-        })
-        .then(({ data }) => {
-            comment.current_user_has_reaction = data.current_user_has_reaction;
-            comment.num_of_reactions = data.num_of_reactions;
         });
 }
 </script>
@@ -164,126 +96,7 @@ function sendCommentReaction(comment) {
             </div>
 
             <DisclosurePanel class="mt-3">
-                <div class="flex gap-2 mb-3">
-                    <a href="javascript:void(0)">
-                        <img
-                            :src="authUser.avatar_url"
-                            class="w-[40px] rounded-full border-2 transition-all hover:border-blue-500"
-                        />
-                    </a>
-                    <div class="flex flex-1">
-                        <InputTextarea
-                            v-model="newCommentText"
-                            placeholder="Enter your comment here"
-                            rows="1"
-                            class="w-full max-h-[160px] resize-none rounded-r-none"
-                        ></InputTextarea>
-                        <IndigoButton
-                            @click="createComment"
-                            class="rounded-l-none w-[100px]"
-                            >Submit</IndigoButton
-                        >
-                    </div>
-                </div>
-                <div>
-                    <div
-                        v-for="comment of post.comments"
-                        :key="comment.id"
-                        class="mb-4"
-                    >
-                        <div class="flex justify-between gap-2">
-                            <div class="flex gap-2">
-                                <a href="javascript:void(0)">
-                                    <img
-                                        :src="comment.user.avatar_url"
-                                        class="w-[40px] rounded-full border-2 transition-all hover:border-blue-500"
-                                    />
-                                </a>
-                                <div>
-                                    <h4 class="font-bold">
-                                        <a
-                                            href="javascript:void(0)"
-                                            class="hover:underline"
-                                        >
-                                            {{ comment.user.name }}
-                                        </a>
-                                    </h4>
-                                    <small class="text-xs text-gray-400">{{
-                                        comment.updated_at
-                                    }}</small>
-                                </div>
-                            </div>
-                            <EditDeleteDropdown
-                                :user="comment.user"
-                                @edit="startCommentEdit(comment)"
-                                @delete="deleteComment(comment)"
-                            />
-                        </div>
-                        <div class="pl-12">
-                            <div
-                                v-if="
-                                    editingComment &&
-                                    editingComment.id === comment.id
-                                "
-                            >
-                                <InputTextarea
-                                    v-model="editingComment.comment"
-                                    placeholder="Enter your comment here"
-                                    rows="1"
-                                    class="w-full max-h-[160px] resize-none"
-                                ></InputTextarea>
-
-                                <div class="flex gap-2 justify-end">
-                                    <button
-                                        @click="editingComment = null"
-                                        class="rounded-r-none text-indigo-500"
-                                    >
-                                        cancel
-                                    </button>
-                                    <IndigoButton
-                                        @click="updateComment"
-                                        class="w-[100px]"
-                                        >update
-                                    </IndigoButton>
-                                </div>
-                            </div>
-                            <ReadMoreReadLess
-                                v-else
-                                :content="comment.comment"
-                                content-class="text-sm flex flex-1"
-                            />
-                            <div class="mt-1 flex gap-2">
-                                <button
-                                    @click="sendCommentReaction(comment)"
-                                    class="flex items-center text-xs text-indigo-500 py-0.5 px-1 rounded-lg"
-                                    :class="[
-                                        comment.current_user_has_reaction
-                                            ? 'bg-indigo-50 hover:bg-indigo-100'
-                                            : 'hover:bg-indigo-50',
-                                    ]"
-                                >
-                                    <HandThumbUpIcon class="w-3 h-3 mr-1" />
-                                    <span class="mr-2">{{
-                                        comment.num_of_reactions
-                                    }}</span>
-                                    {{
-                                        comment.current_user_has_reaction
-                                            ? "unlike"
-                                            : "like"
-                                    }}
-                                </button>
-                                <button
-                                    class="flex items-center text-xs text-indigo-500 py-0.5 px-1 hover:bg-indigo-100 rounded-lg"
-                                >
-                                    <ChatBubbleLeftEllipsisIcon
-                                        class="w-3 h-3 mr-1"
-                                    />
-                                    reply
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <CommentList :post="post" :data="{ comments: post.comments }" />
             </DisclosurePanel>
         </Disclosure>
     </div>
