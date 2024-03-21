692b0865dd207e00732e24e25797e2336f2f4936
bino <hoangpham23052003@gmail.com>
2024-03-21 10:40:37 +0700

    Implement creating group

--- resources/js/Components/app/GroupList.vue
+++ resources/js/Components/app/GroupList.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
 import GroupListItems from "@/Components/app/GroupListItems.vue";
+import GroupModal from "@/Components/app/GroupModal.vue";
+import { ref } from "vue";
+const showNewGroupModal = ref(false);
 </script>
 
 <template>
@@ -19,6 +22,7 @@ import GroupListItems from "@/Components/app/GroupListItems.vue";
                             class="w-6 h-6 transition-all"
                             :class="open ? 'rotate-90 transform' : ''"
                         >
+                            >
                             <path
                                 stroke-linecap="round"
                                 stroke-linejoin="round"
@@ -28,15 +32,30 @@ import GroupListItems from "@/Components/app/GroupListItems.vue";
                     </div>
                 </DisclosureButton>
                 <DisclosurePanel>
+                    <button
+                        @click="showNewGroupModal = true"
+                        class="text-sm bg-indigo-500 hover:bg-indigo-600 text-white rounded py-1 px-2"
+                    >
+                        new group
+                    </button>
                     <GroupListItems />
                 </DisclosurePanel>
             </Disclosure>
         </div>
         <div class="h-full overflow-hidden flex-col hidden lg:flex">
-            <h2 class="text-xl font-bold">My Groups</h2>
+            <div class="flex justify-between">
+                <h2 class="text-xl font-bold">My Groups</h2>
+                <button
+                    @click="showNewGroupModal = true"
+                    class="text-sm bg-indigo-500 hover:bg-indigo-600 text-white rounded py-1 px-2"
+                >
+                    new group
+                </button>
+            </div>
             <GroupListItems />
         </div>
     </div>
+    <GroupModal v-model="showNewGroupModal" />
 </template>
 
 <style scoped></style>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\GroupController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -31,6 +32,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    // Posts
     Route::post('/post', [PostController::class, 'store'])
         ->name('post.create');
     Route::put('/post/{post}', [PostController::class, 'update'])
@@ -41,15 +43,20 @@ Route::middleware('auth')->group(function () {
         ->name('post.download');
     Route::post('/post/{post}/reaction', [PostController::class, 'postReaction'])
         ->name('post.reaction');
-
     Route::post('/post/{post}/comment', [PostController::class, 'createComment'])
         ->name('post.comment.create');
+
+    // Comments
     Route::delete('/comment/{comment}', [PostController::class, 'deleteComment'])
         ->name('comment.delete');
     Route::put('/comment/{comment}', [PostController::class, 'updateComment'])
         ->name('comment.update');
     Route::post('/comment/{comment}/reaction', [PostController::class, 'commentReaction'])
         ->name('comment.reaction');
+
+    // Groups
+    Route::post('/group', [GroupController::class, 'store'])
+        ->name('group.create');
 });
 
 require __DIR__ . '/auth.php';
