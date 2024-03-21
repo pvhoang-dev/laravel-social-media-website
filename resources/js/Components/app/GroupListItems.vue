25577694ebdcfd72cb36f27da184f93ace4f52f3
bino <hoangpham23052003@gmail.com>
2024-03-21 10:45:47 +0700

    Give proper position to new group button

--- resources/js/Components/app/GroupListItems.vue
+++ resources/js/Components/app/GroupListItems.vue
@@ -2,15 +2,26 @@
 import TextInput from "@/Components/TextInput.vue";
 import GroupItem from "@/Components/app/GroupItem.vue";
 import { ref } from "vue";
+import GroupModal from "@/Components/app/GroupModal.vue";
+
 const searchKeyword = ref("");
+const showNewGroupModal = ref(false);
 </script>
 
 <template>
-    <TextInput
-        :model-value="searchKeyword"
-        placeholder="Type to search"
-        class="w-full mt-4"
-    />
+    <div class="flex gap-2 mt-4">
+        <TextInput
+            :model-value="searchKeyword"
+            placeholder="Type to search"
+            class="w-full"
+        />
+        <button
+            @click="showNewGroupModal = true"
+            class="text-sm bg-indigo-500 hover:bg-indigo-600 text-white rounded py-1 px-2 w-[120px]"
+        >
+            New Group
+        </button>
+    </div>
     <div class="mt-3 h-[200px] lg:flex-1 overflow-auto">
         <div v-if="false" class="text-gray-400 text-center p-3">
             You are not joined to any groups
@@ -28,6 +39,7 @@ const searchKeyword = ref("");
             />
         </div>
     </div>
+    <GroupModal v-model="showNewGroupModal" />
 </template>
 
 <style scoped></style>
