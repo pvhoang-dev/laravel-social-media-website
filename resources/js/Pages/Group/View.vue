b6f0a94eb5d523f6ef68164ba62eadeb9a48f71b
bino <hoangpham23052003@gmail.com>
2024-03-25 13:57:47 +0700

    Display about tag in group profile page for guest users

--- resources/js/Pages/Group/View.vue
+++ resources/js/Pages/Group/View.vue
@@ -362,11 +362,7 @@ function updateGroup() {
                         <Tab v-slot="{ selected }" as="template">
                             <TabItem text="Photos" :selected="selected" />
                         </Tab>
-                        <Tab
-                            v-if="isCurrentUserAdmin"
-                            v-slot="{ selected }"
-                            as="template"
-                        >
+                        <Tab v-slot="{ selected }" as="template">
                             <TabItem text="About" :selected="selected" />
                         </Tab>
                     </TabList>
@@ -427,10 +423,13 @@ function updateGroup() {
                             Photos
                         </TabPanel>
                         <TabPanel class="bg-white p-3 shadow">
-                            <GroupForm :form="aboutForm" />
-                            <PrimaryButton @click="updateGroup">
-                                Submit
-                            </PrimaryButton>
+                            <template v-if="isCurrentUserAdmin">
+                                <GroupForm :form="aboutForm" />
+                                <PrimaryButton @click="updateGroup">
+                                    Submit
+                                </PrimaryButton>
+                            </template>
+                            <div v-else v-html="group.about"></div>
                         </TabPanel>
                     </TabPanels>
                 </TabGroup>
