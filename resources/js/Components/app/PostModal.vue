29f5abbc049ac06b8988b933a55a7d24ebb67a83
bino <hoangpham23052003@gmail.com>
2024-03-26 21:09:07 +0700

    Implement accessing open AI API to generate post content

--- resources/js/Components/app/PostModal.vue
+++ resources/js/Components/app/PostModal.vue
@@ -17,6 +17,7 @@ import PostUserHeader from "@/Components/app/PostUserHeader.vue";
 import { useForm, usePage } from "@inertiajs/vue3";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { isImage } from "@/helpers.js";
+import axiosClient from "@/axiosClient.js";
 
 const editor = ClassicEditor;
 const editorConfig = {
@@ -59,6 +60,7 @@ const attachmentExtensions = usePage().props.attachmentExtensions;
 const attachmentFiles = ref([]);
 const attachmentErrors = ref([]);
 const formErrors = ref({});
+const aiButtonLoading = ref(false);
 const form = useForm({
     body: "",
     group_id: null,
@@ -193,6 +195,27 @@ function undoDelete(myFile) {
         (id) => myFile.id !== id
     );
 }
+
+function getAIContent() {
+    console.log(1);
+    if (!form.body) {
+        console.log(2);
+        return;
+    }
+    aiButtonLoading.value = true;
+    axiosClient
+        .post(route("post.aiContent"), {
+            prompt: form.body,
+        })
+        .then(({ data }) => {
+            form.body = data.content;
+            aiButtonLoading.value = false;
+        })
+        .catch((err) => {
+            console.log(err);
+            aiButtonLoading.value = false;
+        });
+}
 </script>
 
 <template>
@@ -253,11 +276,57 @@ function undoDelete(myFile) {
                                     >
                                         {{ formErrors.group_id }}
                                     </div>
-                                    <ckeditor
-                                        :editor="editor"
-                                        v-model="form.body"
-                                        :config="editorConfig"
-                                    ></ckeditor>
+                                    <div class="relative group">
+                                        <ckeditor
+                                            :editor="editor"
+                                            v-model="form.body"
+                                            :config="editorConfig"
+                                        ></ckeditor>
+
+                                        <button
+                                            @click="getAIContent"
+                                            :disabled="aiButtonLoading"
+                                            class="absolute right-1 top-12 w-8 h-8 p-1 rounded bg-indigo-500 hover:bg-indigo-600 text-white flex justify-center items-center transition-all opacity-0 group-hover:opacity-100 disabled:cursor-not-allowed disabled:bg-indigo-400 disabled:hover:bg-indigo-400"
+                                        >
+                                            <svg
+                                                v-if="aiButtonLoading"
+                                                class="animate-spin h-4 w-4 text-white"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                            >
+                                                <circle
+                                                    class="opacity-25"
+                                                    cx="12"
+                                                    cy="12"
+                                                    r="10"
+                                                    stroke="currentColor"
+                                                    stroke-width="4"
+                                                ></circle>
+                                                <path
+                                                    class="opacity-75"
+                                                    fill="currentColor"
+                                                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                                ></path>
+                                            </svg>
+
+                                            <svg
+                                                v-else
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                stroke-width="1.5"
+                                                stroke="currentColor"
+                                                class="w-4 h-4"
+                                            >
+                                                <path
+                                                    stroke-linecap="round"
+                                                    stroke-linejoin="round"
+                                                    d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
+                                                />
+                                            </svg>
+                                        </button>
+                                    </div>
 
                                     <div
                                         v-if="showExtensionsText"
