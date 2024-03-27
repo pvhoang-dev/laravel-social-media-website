75c8b4ba6aa949cb6b203deb2a09d2fb04959f93
bino <hoangpham23052003@gmail.com>
2024-03-27 19:16:37 +0700

    Implement switch to dark/light mode and saved the mode in localStorage

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,11 +1,12 @@
 <script setup>
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import ApplicationLogo from "@/Components/ApplicationLogo.vue";
 import Dropdown from "@/Components/Dropdown.vue";
 import DropdownLink from "@/Components/DropdownLink.vue";
 import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
 import { Link, router, usePage } from "@inertiajs/vue3";
 import TextInput from "@/Components/TextInput.vue";
+import { MoonIcon } from "@heroicons/vue/24/solid";
 
 const showingNavigationDropdown = ref(false);
 const keywords = ref(usePage().props.search || "");
@@ -13,6 +14,17 @@ const authUser = usePage().props.auth.user;
 
 function search() {
     router.get(route("search", encodeURIComponent(keywords.value)));
+}
+
+function toggleDarkMode() {
+    const html = window.document.documentElement;
+    if (html.classList.contains("dark")) {
+        html.classList.remove("dark");
+        localStorage.setItem("darkMode", "0");
+    } else {
+        html.classList.add("dark");
+        localStorage.setItem("darkMode", "1");
+    }
 }
 </script>
 
@@ -26,7 +38,7 @@ function search() {
             <!-- Primary Navigation Menu -->
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div class="flex items-center justify-between gap-4 h-16">
-                    <div class="flex">
+                    <div class="flex mr-2">
                         <!-- Logo -->
                         <div class="shrink-0 flex items-center">
                             <Link :href="route('dashboard')">
@@ -37,13 +49,16 @@ function search() {
                         </div>
                     </div>
 
-                    <div class="flex-1">
+                    <div class="flex items-center gap-3 flex-1">
                         <TextInput
                             v-model="keywords"
                             placeholder="Search on the website"
                             class="w-full"
                             @keyup.enter="search"
                         />
+                        <button @click="toggleDarkMode" class="dark:text-white">
+                            <MoonIcon class="w-5 h-5" />
+                        </button>
                     </div>
 
                     <div class="hidden sm:flex sm:items-center">
