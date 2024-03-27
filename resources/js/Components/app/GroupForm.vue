9f27f340d7503e663da1c149760f1a1a07b96fa5
bino <hoangpham23052003@gmail.com>
2024-03-27 19:41:40 +0700

    Fix UI bugs in dark mode in group view page

--- resources/js/Components/app/GroupForm.vue
+++ resources/js/Components/app/GroupForm.vue
@@ -28,7 +28,7 @@ const editorConfig = {
 </script>
 
 <template>
-    <div class="mb-3">
+    <div class="mb-3 dark:text-gray-100">
         <label>Group Name</label>
         <TextInput
             type="text"
@@ -39,14 +39,14 @@ const editorConfig = {
         />
     </div>
 
-    <div class="mb-3">
+    <div class="mb-3 dark:text-gray-100">
         <label>
             <Checkbox name="remember" v-model:checked="form.auto_approval" />
             Enable Auto Approval
         </label>
     </div>
 
-    <div class="mb-3">
+    <div class="mb-3 dark:text-gray-100">
         <label>About Group</label>
         <ckeditor
             :editor="editor"
