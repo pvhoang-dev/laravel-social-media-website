fcc40f3c6e9aa79e9227872e64a9c3ab4f066c76
bino <hoangpham23052003@gmail.com>
2024-05-16 16:31:35 +0700

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+    server: {
+        hmr: {
+            host: "localhost",
+        },
+        port: 3000,
+        host: true,
+    },
+    // plugins: [
+    //     vue(),
+    //     laravel({
+    //         input: ["resources/css/app.css", "resources/js/app.js"],
+    //         refresh: true,
+    //     }),
+    // ],
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: "resources/js/app.js",
             refresh: true,
         }),
         vue({
