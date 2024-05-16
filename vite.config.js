e3fc76323a43e391661a1568c894567209e9feaf
bino <hoangpham23052003@gmail.com>
2024-05-16 17:38:13 +0700

    docker

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+    server: {
+        host: "0.0.0.0",
+        port: 3000,
+        watch: {
+            usePolling: true,
+        },
+        hmr: {
+            host: "localhost",
+            port: 3000,
+        },
+    },
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: "resources/js/app.js",
             refresh: true,
         }),
         vue({
