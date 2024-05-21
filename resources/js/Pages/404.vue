5207130c5e5528fe98533b7325c6a49217b1026a
bino <hoangpham23052003@gmail.com>
2024-05-21 21:05:29 +0700

    404 page

---
+++ resources/js/Pages/404.vue
@@ -0,0 +1,16 @@
+<template>
+    <GuestLayout>
+        <div class="text-center">
+            <h1 class="text-6xl">404</h1>
+            <p class="text-lg">Page not found</p>
+            <p>
+                We could not find the page you were looking for. Meanwhile, you
+                may <router-link to="/">return to dashboard</router-link>.
+            </p>
+        </div>
+    </GuestLayout>
+</template>
+
+<script setup>
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+</script>
